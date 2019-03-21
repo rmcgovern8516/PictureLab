aaeafe219d18b4859a159e337254ca00afa8d949
rchu6120 <rchu6120@bths.edu>
2019-03-21 08:16:36 -0400

    3/21/19

--- classes/IntArrayWorker.java
+++ classes/IntArrayWorker.java
@@ -98,5 +98,17 @@ public class IntArrayWorker
       }
     }
   }
+
+  public int getCount(int num){
+    int count = 0;
+    for (int row=0; row<matrix.length; row++){
+      for (int col=0; col<matrix[0].length; col++){
+        if (matrix[row][col] == num){
+          count++;
+        }
+      }
+    }
+    return count;
+  }
  
 }
